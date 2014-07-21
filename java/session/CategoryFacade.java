e132e3f90e522e14e7b13f891caba0de51eb5521
unknown <t_beher@CCS1HF0VZ1.ads.autodesk.com>
2014-07-21 14:21:43 -0700

    This version does NOT work. Exception - java.lang.IllegalStateException: Unable to retrieve EntityManagerFactory for unitName AffableBeanPU

--- java/session/CategoryFacade.java
+++ java/session/CategoryFacade.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package session;
 
 import entity.Category;
@@ -10,10 +5,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-/**
- *
- * @author tgiunipero
- */
 @Stateless
 public class CategoryFacade extends AbstractFacade<Category> {
     @PersistenceContext(unitName = "AffableBeanPU")
